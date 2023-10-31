a0cd43c279a8944af5252026f8e14e875985fce7
Yepthatsit <126805460+Yepthatsit@users.noreply.github.com>
2023-10-31 21:52:09 +0100

    Update lab13v2.py

--- lab13v2.py
+++ lab13v2.py
@@ -1,16 +1,21 @@
+#import niezbędnych bibliotek
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 from scipy import stats
-t = np.linspace(0,10)
+
+#tablica zawierająca czasy spadku kulki zmierzone na odcinkach o długości 80 cm.
 tzm = np.array([7.16,7.75,8.34,8.93,9.52,10.11,10.7,11.29,11.88,12.47])
+
+#deklaracja parametrów badanej kulki oraz wyznaczenie prędkości granicznej
 m = 0.000374
 d = 0.00394
 mw = 1/6*math.pi*d**3*1261
 k = (6)*math.pi*d*0.519*(1+2.4*(d/0.039))
-a = m/k
 vgr = (9.81*(m-mw))/k
-v  =  vgr - vgr*math.e**(-t/a)
-vsrnaprzedz = 0.8/tzm
-print(vsrnaprzedz)
-print(stats.ttest_1samp(vsrnaprzedz,vgr))
+
+vsrnaprzedz = 0.8/tzm #utworzenie tablicy zawierającej prędkości wyznaczone przy pomocy tablicy tzm
+
+print(vsrnaprzedz)# prezentacja wyznaczonych prędkości
+
+print(stats.ttest_1samp(vsrnaprzedz,vgr))  # przeprowadzenie testy statystycznego oraz wypisanie jego rezultatów (TtestResult(statistic=3.136681058362772, pvalue=0.011991948710394872, df=9))
